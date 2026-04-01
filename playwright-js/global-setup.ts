1c989adeba08a2d26173dc1e4b68a2cd68479aec
maxnewsom <maxjosephnewsom@gmail.com>
2026-04-01 09:45:24 -0500

    Add debug logging to global setup

--- playwright-js/global-setup.ts
+++ playwright-js/global-setup.ts
@@ -3,5 +3,10 @@ import { sauceVisualSetup } from "@saucelabs/visual-playwright";
 
 
 export default async function globalSetup(config: FullConfig){
+    if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
+        console.log('Credentials check:');
+        console.log('  SAUCE_USERNAME is ' + (process.env.SAUCE_USERNAME ? 'set' : 'NOT set'));
+        console.log('  SAUCE_ACCESS_KEY is ' + (process.env.SAUCE_ACCESS_KEY ? 'set' : 'NOT set'));
+    }
     await sauceVisualSetup();
 }
